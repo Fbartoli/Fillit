4e5fb7a9e2b63d92143de08632479e8f2e9a686f
Frank TRUJILLO <ftrujill@e1r4p4.42.fr>
2019-01-05 20:21:11 +0100

    modified ft_sqrt

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: flbartol <flbartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/05 19:35:35 by flbartol          #+#    #+#             */
-/*   Updated: 2019/01/05 19:39:16 by flbartol         ###   ########.fr       */
+/*   Updated: 2019/01/05 20:09:30 by ftrujill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,6 @@ int			ft_sqrt(int nb)
 			root++;
 			square = root * root;
 		}
-		if (square == nb)
-			return (root);
-		else
-			return (0);
+		return (root);
 	}
 }
