b9731f262aaf825383556c82cda46b3fd32eaa56
Florent BARTOLI <flbartol@e1r4p6.42.fr>
2019-01-05 19:33:03 +0100

    refactoring recursive.c

--- recursive.c
+++ recursive.c
@@ -6,14 +6,37 @@
 /*   By: flbartol <flbartol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 14:30:58 by flbartol          #+#    #+#             */
-/*   Updated: 2019/01/05 18:09:47 by ftrujill         ###   ########.fr       */
+/*   Updated: 2019/01/05 19:30:05 by flbartol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "./libft/libft.h"
 
-int		solver(t_etris tetris[27], t_map *map)
+static void		put_and_reset(t_etris *tetris, t_map *map, int *t)
+{
+	map->position_piece[*t][0] = map->current_xy[0];
+	map->position_piece[*t][1] = map->current_xy[1];
+	put_in_map(tetris, map);
+	map->current_xy[0] = 0;
+	map->current_xy[1] = 0;
+	*t += 1;
+}
+
+static void		increase_size(t_map *map)
+{
+	map->size_map += 1;
+	map->current_xy[0] = 0;
+	map->current_xy[1] = 0;
+}
+
+static void		move_down(t_map *map)
+{
+	map->current_xy[0] += 1;
+	map->current_xy[1] = 0;
+}
+
+int				solver(t_etris tetris[27], t_map *map)
 {
 	int *t;
 
@@ -21,39 +44,21 @@ int		solver(t_etris tetris[27], t_map *map)
 	while (*t != map->nb_tot_tetris)
 	{
 		if (tetris[*t].length > map->size_map - map->current_xy[1])
-		{
-			map->current_xy[0] += 1;
-			map->current_xy[1] = 0;
-		}
+			move_down(map);
 		if (tetris[*t].width > map->size_map - map->current_xy[0])
 		{
 			*t -= 1;
-			delete_tetris(&tetris[*(t)], map, *t);
+			delete_tetris(&tetris[*t], map, *t);
 			map->current_xy[0] = map->position_piece[*t][0];
 			map->current_xy[1] = map->position_piece[*t][1] + 1;
-			if (*t == 0 /*&& tetris[0].length > map->size_map - map->current_xy[1]*/ && tetris[0].width == map->size_map - map->current_xy[0]) 
-			{
-				if (map->size_map > 15)
-					return(-1);
-				map->size_map += 1;
-				map->current_xy[0] = 0;
-				map->current_xy[1] = 0;
-			}
+			if (*t == 0
+				&& tetris[0].width == map->size_map - map->current_xy[0])
+				increase_size(map);
 		}
-		else if (is_available_map(&(tetris[*t]), map) == -1
-				 /*&& map->current_xy[1] < map->size_map*/)
+		else if (is_available_map(&(tetris[*t]), map) == -1)
 			map->current_xy[1] += 1;
 		else
-		{
-			map->position_piece[*t][0] = map->current_xy[0];
-			map->position_piece[*t][1] = map->current_xy[1];
-			put_in_map(&tetris[*t], map);
-			map->current_xy[0] = 0;
-			map->current_xy[1] = 0;
-			*t += 1;
-		
-		}
-	//printf("t: %d, size: %d\n, xde0: %d, yde0: %d", *t, map->size_map, map->position_piece[0][1], map->position_piece[0][0]);
+			put_and_reset(&(tetris[*t]), map, t);
 	}
 	return (0);
 }
